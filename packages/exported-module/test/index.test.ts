1dae8f12f111e85715d9f11c5e8af0226537d191
Takahito Urai <uraitakahito@gmail.com>
2025-01-02 20:04:53 +0900

    Refactor import statements to use consistent spacing and single quotes

--- packages/exported-module/test/index.test.ts
+++ packages/exported-module/test/index.test.ts
@@ -1,8 +1,8 @@
-import test from "node:test";
-import assert from "node:assert";
+import test from 'node:test';
+import assert from 'node:assert';
 
-import {hello} from "../index.js";
+import { hello } from '../index.js';
 
-test("Success test", async (t) => {
-  assert.strictEqual(hello, "Hello, ES Module!");
+test('Success test', async () => {
+  assert.strictEqual(hello, 'Hello, ES Module!');
 });
