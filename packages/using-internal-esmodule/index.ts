4298205d19d30e60c725484a6554ce8c2bf77a4b
Takahito Urai <uraitakahito@gmail.com>
2025-01-08 14:54:27 +0900

    Update index.ts with ES module import examples and file extension notes

--- packages/using-internal-esmodule/index.ts
+++ packages/using-internal-esmodule/index.ts
@@ -1,3 +1,18 @@
-import hello from '../exported-module/index.js';
+/* eslint-disable no-console */
 
-console.log(hello); // Hello, world!
+// MEMO:
+// When importing ES modules without using a bundler or transpiler, file extensions are required:
+//   https://nodejs.org/api/esm.html#esm_mandatory_file_extensions
+
+//
+// Named import without `default export`:
+//
+// Good:
+import { named1 } from '../named-exports-no-default/index.js';
+import * as namespaceObject1 from '../named-exports-no-default/index.js';
+// Bad: SyntaxError
+// import defaultExport1 from '../named-exports-no-default/index.esm.js';
+
+// Named import without `default export`:
+console.log(named1);
+console.log(namespaceObject1.named1, namespaceObject1.named2);
