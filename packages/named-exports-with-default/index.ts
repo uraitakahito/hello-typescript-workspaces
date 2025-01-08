24b1d52fc508eb720795eb4389e9209e249595cc
Takahito Urai <uraitakahito@gmail.com>
2025-01-08 16:24:21 +0900

    Add named-exports-with-default package with TypeScript configuration and usage examples

---
+++ packages/named-exports-with-default/index.ts
@@ -0,0 +1,9 @@
+//
+// ALERT:
+//   DO NOT USE default export
+//   https://engineering.linecorp.com/ja/blog/you-dont-need-default-export
+//
+export const named3 = 'named3';
+const named4 = 'named4';
+
+export default named4;
