673c22e233911d98e743b2fcd962656d3ab36b4a
Takahito Urai <uraitakahito@gmail.com>
2025-01-09 10:45:07 +0900

    Add @uraitakahito/hello-conditional-exports dependency and create import example

---
+++ packages/local-import-checker/import-conditional-exports.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-console */
+import * as hello from '@uraitakahito/hello-conditional-exports';
+import * as hellolib from '@uraitakahito/hello-conditional-exports/lib';
+
+console.log(hello.say());
+console.log(hellolib.say());
